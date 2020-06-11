fea710451c0073d35dfcedd6b5e6f9d4efef5c4b
judy.gab <ivditigabeskiria@gmail.com>
2020-06-10 23:31:57 -0400

    added transition for the menu

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { CSSTransition } from 'react-transition-group';
 
 function App() {
   return (
@@ -15,9 +16,17 @@ function App() {
 
 function DropdownMenu() {
 
+  const [activeMenu, setActiveMenu] = useState('main');
+  const [menuHeight, setMenuHeight] = useState(null);
+
+  function calcHeight(el) {
+    const height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
   function DropdownItem(props) {
     return (
-      <a href="#" className="menu-item">
+      <a href="#" className="menu-item" onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}>
         <span className="icon-left">{props.leftIcon}</span>
         {props.children}
         <span className="icon-right">{props.rightIcon}</span>
@@ -25,10 +34,32 @@ function DropdownMenu() {
     )
   }
 
+  //CSS Transition adds or removes css classes to the element based on the state of transition
   return (
-    <div className="dropdown">
-      <DropdownItem>My Profile</DropdownItem>
-      <DropdownItem leftIcon="✌️"></DropdownItem>
+    <div className="dropdown" style={{ height: menuHeight }}>
+      <CSSTransition
+        in={activeMenu === 'main'}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-primary"
+        onEnter={calcHeight}
+        >
+        <div className="menu">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem leftIcon="✌️"></DropdownItem>
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={activeMenu === 'settings'}
+        unmountOnExit
+        timeout={500}
+        classNames="menu-secondary"
+        >
+        <div className="menu">
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem leftIcon="✌️" goToMenu="main" />
+        </div>
+      </CSSTransition>
     </div>
   );
 }
